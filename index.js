a3c928a7bee107d39c4a74c18de3c2c6a08cb6b4
Apostolis Konoupakis <konoupakis@gmail.com>
2015-10-20 22:58:36 +0300

    .

---
+++ index.js
@@ -0,0 +1,12 @@
+var path = require('path');
+
+module.exports = {
+    getPath: function (paths) {
+
+        var dir = __dirname;
+        if (dir.indexOf('node_modules') !== -1)
+            dir = dir.substring(0, dir.indexOf('node_modules'));
+
+        return path.join(dir, paths);
+    }
+};
