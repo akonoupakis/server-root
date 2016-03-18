b3ccb8db3dc77ed1e84c0e155a451c52a7862097
Apostolis Konoupakis <konoupakis@gmail.com>
2016-03-18 23:24:27 +0200

    code style standard

--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
-var assert = require('assert');
-var path = require('path');
-var server = require('../index.js');
+/*global describe,it */
+
+var assert = require('assert')
+var path = require('path')
+var server = require('../index.js')
 
 describe('mapPath', function () {
-    
-    it('should return full path to "folder/file.txt"', function () {
-        assert.equal(server.mapPath('folder/file.txt'), path.join(server.root, 'folder/file.txt'));
-    });
-    
-    it('should return full path to "./folder/file.txt"', function () {
-        assert.equal(server.mapPath('./folder/file.txt'), path.join(server.root, 'folder/file.txt'));
-    });
-    
-    it('should return full path to "../folder/file.txt"', function () {
-        assert.equal(server.mapPath('../folder/file.txt'), path.join(server.root, '../folder/file.txt'));
-    });
-    
-});
+  it('should return full path to "folder/file.txt"', function () {
+    assert.equal(server.mapPath('folder/file.txt'), path.join(server.root, 'folder/file.txt'))
+  })
+
+  it('should return full path to "./folder/file.txt"', function () {
+    assert.equal(server.mapPath('./folder/file.txt'), path.join(server.root, 'folder/file.txt'))
+  })
+
+  it('should return full path to "../folder/file.txt"', function () {
+    assert.equal(server.mapPath('../folder/file.txt'), path.join(server.root, '../folder/file.txt'))
+  })
+})
